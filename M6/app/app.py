59da49806cfbdcefc8d729c2c989ac9d77dce243
breakingbaldMO <33108238+breakingbaldMO@users.noreply.github.com>
2020-07-03 09:37:29 -0500

    added M6 folder for docker

---
+++ M6/app/app.py
@@ -0,0 +1,140 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect, url_for, session
+from functools import wraps
+import db
+
+
+app = Flask(__name__)
+# app.secret_key = b'gdfgdrggfg1453'
+app.secret_key = b'gdfgdrggfg1453'
+
+
+def check_admin():
+    return 'admin' in session
+
+
+def current_user():
+    return 'username' in session
+
+
+def requires_admin(view):
+    @wraps(view)
+    def decorated(**kwargs):
+        if not check_admin():
+            return redirect('/login')
+        return view(**kwargs)
+    return decorated
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():        
+    return "Welcome to the Image Gallery User Database. For database admin functions please navigate to elisamek.codes/admin"
+
+
+@app.route('/invalidLogin')
+def invalidLogin():
+    return "Invalid login"
+
+
+@app.route('/validLogin')
+def validLogin():
+    return "Valid login"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    db.connect()
+    if request.method == 'POST':
+        password = db.select_password(request.form["username"])
+       
+        form_pass = request.form["password"]
+        if password != form_pass:
+            return redirect('/login')
+            
+        else:
+            session['username'] = request.form["username"]
+            return redirect('/validLogin')
+    else:
+        db.close()
+        return render_template('login.html')
+
+
+@app.route('/debugSession')
+def debugSession():
+    result = " "
+    for key,value in session.items():
+        result += key+"->"+str(value)+"<br />"
+    return result
+
+@app.route('/inc')
+def inc():
+    if 'value' not in session:
+        session['value'] = 0
+    session['value'] = session['value'] + 1
+    return "<h1>"+str(session['value']+"</h1>")
+
+
+@requires_admin
+@app.route('/admin', methods=["GET", "POST"])
+def index():
+    db.connect()
+    return render_template('index.html', username=db.select_all_usernames("users"))
+
+
+@requires_admin
+@app.route('/admin/edit/<username>')
+def edit(username):
+     return render_template('edit.html', username=username)
+
+
+@app.route('/admin/edit/modify', methods=['POST'])
+def modify():
+    db.connect()
+    username = request.form['username']
+    password = request.form['password']
+    full_name = request.form['full_name']
+    db.edit_user(username, password, full_name)
+    res = db.select_user_info(username, 'users') 
+    db.close()
+    return render_template('modify.html', user_info=res)
+
+
+@requires_admin    
+@app.route('/admin/addUser')
+def addUser():
+    return render_template('addUser.html')
+
+
+@app.route('/admin/addUser/added', methods=['POST'])
+def added():
+    db.connect()
+    username = request.form['username']
+    password = request.form['password']
+    full_name = request.form['full_name']
+    db.add_user(username, password, full_name)
+    res = db.select_user_info(username, 'users') 
+    db.close()
+    return render_template('added.html', user_info=res)
+
+
+@app.route('/admin/edit/delete', methods=['POST'])
+def delete():
+    db.connect()
+    username = request.form['username']    
+    username = username.strip()
+    db.delete_user(username)
+    db.close()    
+    return render_template('deleteSuccessful.html',username= username)
+
+
+@app.route('/admin/delete', methods=['POST'])
+def main_delete():
+    db.connect()
+    username = request.form['username']    
+    username = username.strip()
+    db.delete_user(username)
+    return render_template('deleteSuccessful.html',username= username)
+
+
---
+++ M6/app/db.py
@@ -0,0 +1,103 @@
+import psycopg2
+import json
+
+connection = None
+
+def close():
+    connection.close()
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+def connect():
+    global connection
+    connection = psycopg2.connect(host='database-2.cv1n9oljqdta.us-east-1.rds.amazonaws.com', dbname='postgres', user='postgres', password='password')
+    connection.set_session(autocommit=True)
+
+
+def execute(query, args=None):
+    global connection
+    cursor = connection.cursor()
+    if not args:
+        cursor.execute(query)
+    else:
+        cursor.execute(query, args)
+    return cursor
+
+
+def add_user(username, password, full_name):
+    cursor = connection.cursor()
+    username = username.strip()
+    try:
+        execute("""
+        INSERT into users (username, password, full_name) VALUES (%s, %s, %s);
+        """, (username, password, full_name))
+
+
+    except Exception as error:
+        print("Error: a user with username '" + username + "' already exists\n")
+
+
+def edit_user(user_to_edit, password, full_name):
+    try:
+        if password != "-":
+                execute("UPDATE users SET password='" + password + "' WHERE username='" + user_to_edit + "';")
+
+
+    except Exception as error:
+                print("Error updating password\n")
+    try:
+        if full_name != "-":
+                execute("UPDATE users SET full_name='" + full_name + "' WHERE username='" + user_to_edit + "';")
+
+
+    except Exception as error:
+                print("Error updating full name\n")
+
+
+def delete_user(user_to_delete):
+    try:
+        execute("DELETE FROM users WHERE username='" + user_to_delete + "\';")
+        connection.commit()
+    except Exception as error:
+            print("Error deleting username\n")
+
+
+def select_all(table):
+    res = execute('select * from ' + table).fetchall()
+    return res
+
+def select_password(username):
+    res = execute("select password from users where username='" + username + "\'").fetchone()
+    if res:
+   	 for row in res:
+       	     password = row
+   	 return password
+
+def select_user_info(username, table):
+    res = execute("select * from " + table + " where username='" + username + "';").fetchall()
+    return res
+
+def select_user_info_string(username, table):
+    cursor = execute("select * from " + table + " where username='" + username + "';").fetchall()
+    row = cursor.fetchone()
+    if row is None:
+        return None
+    else:
+        res =  row[1]
+        return res
+
+def select_all_usernames(table):
+    res = execute('select username from ' + table).fetchall()
+    return res
+
+
+def main():
+    connect()
+    res = execute('select * from users')
+    for row in res:
+        print(row)
+
+if __name__ == '__main__':
+    main()
