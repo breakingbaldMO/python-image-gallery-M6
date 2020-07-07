9525107fac8fc54299356810defd353eb7b4fa10
breakingbaldMO <33108238+breakingbaldMO@users.noreply.github.com>
2020-07-06 21:19:17 -0500

    Update M6 repo

--- M6/app/create_tables.sql
+++ M6/app/create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (username VARCHAR(50), password VARCHAR(50), full_name VARCHAR(50));
-CREATE TABLE images (user VARCHAR(50), key VARCHAR(200));
+CREATE TABLE images (username VARCHAR(50), key VARCHAR(200));
 INSERT INTO users VALUES ('dongji', 'cpsc4973', 'dongji grader');
 INSERT INTO users VALUES ('admin', 'admin', 'admin');
-EXIT
+EXIT;
