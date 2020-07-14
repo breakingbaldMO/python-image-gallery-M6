4ebab98faa34db0a481ecfe368d819ba9447f75c
EC2 Default User <ec2-user@ip-172-31-74-162.ec2.internal>
2020-07-14 17:23:47 +0000

    update

--- M6/app/create_tables.sql
+++ M6/app/create_tables.sql
@@ -2,4 +2,4 @@ CREATE TABLE users (username VARCHAR(50), password VARCHAR(50), full_name VARCHA
 CREATE TABLE images (username VARCHAR(50), key VARCHAR(200));
 INSERT INTO users VALUES ('dongji', 'cpsc4973', 'dongji grader');
 INSERT INTO users VALUES ('admin', 'admin', 'admin');
-EXIT;
+
